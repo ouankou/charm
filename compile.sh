43d157e63aa04131816c6e9b44d12113dd172d72
Anjia Wang <anjiawang@gmail.com>
2020-07-24 03:54:37 +0000

    Use GCC 10 instead GCC 7

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build Charm++ and its AMR library.
 
-./build charm++ multicore-linux-x86_64
+./build charm++ multicore-linux-x86_64 gcc-10 -j6
 
 cd src/libs/ck-libs/amr/
 make clean
